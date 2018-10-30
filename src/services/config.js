fea291650abac100e948c270b4d44943a50a3f53
Sachin Ahuja <sachinahj@gmail.com>
2018-10-29 22:14:13 -0700

    us west 2

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
 export const TS_URL_ID = "lvw3kntcca";
 export const TS_API_KEY = "iCSmZxiNpz1loF9a0le861QiZ8dhQ9Th7yOceLO0";
 export const TS_LIMIT = 100;
-export const TS_URL = `https://${TS_URL_ID}.execute-api.us-west-1.amazonaws.com/dev`;
+export const TS_URL = `https://${TS_URL_ID}.execute-api.us-west-2.amazonaws.com/dev`;
