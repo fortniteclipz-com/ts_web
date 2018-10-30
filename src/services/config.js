765505bda62f97328ade28a69aa671f7305b3c68
Sachin Ahuja <sachinahj@gmail.com>
2018-10-30 02:05:21 -0700

    update config

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-export const TS_URL_ID = "xnxfk334lg";
-export const TS_API_KEY = "meZbKRjdl26WoDJfa3aR9jUKVH7Nq5N3BSXcjNB5";
+export const TS_URL_ID = "ox4gzl1xn7";
+export const TS_API_KEY = "VCs5sfOILU4VB39sIRhj55bZVW8HzqupXTip7n3d";
 export const TS_LIMIT = 100;
 export const TS_URL = `https://${TS_URL_ID}.execute-api.us-west-2.amazonaws.com/dev`;
