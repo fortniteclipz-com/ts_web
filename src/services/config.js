7ae67c297782b647439470b9b2b4eed0cda7df75
Sachin Ahuja <sachinahj@gmail.com>
2018-11-24 03:46:17 -0600

    update config

--- src/services/config.js
+++ src/services/config.js
@@ -2,12 +2,12 @@ const config = {};
 
 config.aws = {};
 config.aws.apiGateway = {
-    url: "https://z0gollrmpj.execute-api.us-west-2.amazonaws.com/dev",
+    url: "https://v2uxgnhge7.execute-api.us-west-2.amazonaws.com/dev",
 };
 config.aws.cognito = {
     region: "us-west-2",
-    userPoolID: "us-west-2_YJIuN6odA",
-    appClientID: "6onm8glm036s1o93ts8qb7h949"
+    userPoolID: "us-west-2_FdQiY1UDH",
+    appClientID: "6ceg7mr24737fmc30k426v0c71"
 };
 
 export default config;
