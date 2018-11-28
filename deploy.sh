945c21db3ebcd84f70f4bf8c74e6cd468e3a6bc4
Sachin Ahuja <sachinahj@gmail.com>
2018-11-27 18:53:24 -0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 yarn build
-aws s3 sync ./build s3://dev.fortniteclipz.com
-aws s3 sync ./build s3://www.fortniteclipz.com
+
+if [ $1 == 'prod' ]; then
+    echo "deloying to prod"
+    aws s3 sync ./build s3://www.fortniteclipz.com
+else
+    echo "deloying to dev"
+    aws s3 sync ./build s3://dev.fortniteclipz.com
+fi
