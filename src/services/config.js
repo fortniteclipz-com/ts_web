6ed3b4a3f60c47a598bd5971e7c8567c1df968f0
Sachin Ahuja <sachinahj@gmail.com>
2018-11-30 00:46:27 -0800

    config update

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 let config = {
     aws: {
         apiGateway: {
-            url: "https://kt1zau9yaf.execute-api.us-east-2.amazonaws.com/dev",
+            url: "https://bpegtv22ak.execute-api.us-east-2.amazonaws.com/dev",
         },
         cognito: {
             region: "us-east-2",
