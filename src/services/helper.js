27ff93e0112e0c9290272b923afdf69abb803f43
Sachin Ahuja <sachinahj@gmail.com>
2018-12-01 14:49:43 -0800

    enhance clip creator

--- src/services/helper.js
+++ src/services/helper.js
@@ -18,38 +18,40 @@ helper.createClips = function(stream, streamMoments) {
     const clips = [];
     let include = 0;
     let clipMoments = [];
-    streamMoments.forEach(function(stream_moment) {
+    streamMoments.forEach(function(stream_moment, index) {
         if (!clipMoments.length || stream_moment.time - clipMoments[clipMoments.length - 1].time <= 5) {
             clipMoments.push(stream_moment);
-        } else {
-            let time_in = clipMoments[0].time - 4;
-            if (time_in < 0) {
-                time_in = 0;
+            if (index != streamMoments.length - 1) {
+                return
             }
-            let time_out = clipMoments[clipMoments.length - 1].time + 1;
-            if (time_out > stream.duration) {
-                time_out = stream.duration;
-            }
-            let time_min = clipMoments[0].time - 10;
-            if (time_min < 0) {
-                time_min = 0;
-            }
-            let time_max = clipMoments[clipMoments.length - 1].time + 10;
-            if (time_max > stream.duration) {
-                time_max = stream.duration;
-            }
-
-            clips.push({
-                uuid: uuidv4(),
-                include: include++ < 5,
-                time_in: parseInt(time_in),
-                time_out: parseInt(time_out),
-                time_min: parseInt(time_min),
-                time_max: parseInt(time_max),
-                moments: clipMoments,
-            });
-            clipMoments = [];
         }
+        let time_in = clipMoments[0].time - 4;
+        if (time_in < 0) {
+            time_in = 0;
+        }
+        let time_out = clipMoments[clipMoments.length - 1].time + 1;
+        if (time_out > stream.duration) {
+            time_out = stream.duration;
+        }
+        let time_min = clipMoments[0].time - 10;
+        if (time_min < 0) {
+            time_min = 0;
+        }
+        let time_max = clipMoments[clipMoments.length - 1].time + 10;
+        if (time_max > stream.duration) {
+            time_max = stream.duration;
+        }
+
+        clips.push({
+            uuid: uuidv4(),
+            include: include++ < 5,
+            time_in: parseInt(time_in),
+            time_out: parseInt(time_out),
+            time_min: parseInt(time_min),
+            time_max: parseInt(time_max),
+            moments: clipMoments,
+        });
+        clipMoments = [stream_moment];
     });
 
     return clips;
