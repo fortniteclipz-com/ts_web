b92a6c8719c8a518c13482279370eee44e82e2ff
Sachin Ahuja <sachinahj@gmail.com>
2018-12-20 02:24:04 -0800

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ yarn build
 if [ $1 == 'prod' ]; then
     echo "deloying to prod"
     aws s3 sync ./build s3://www.fortniteclipz.com
-else
-    echo "deloying to dev"
-    aws s3 sync ./build s3://dev.fortniteclipz.com
 
     IFS='. ' read -r -a array <<< $(git describe --tags $(git rev-list --tags --max-count=1))
     array[2]="$((array[2] + 1))"
     function join { local IFS="$1"; shift; echo "$*"; }
     version=$(join . ${array[@]})
     git tag -a $version -m ""
+else
+    echo "deloying to dev"
+    aws s3 sync ./build s3://dev.fortniteclipz.com
 fi
