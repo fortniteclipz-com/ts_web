4be5cf2a9a25ed8b2629bd02ac89dc16f1e6b296
Sachin Ahuja <sachinahj@gmail.com>
2018-12-28 14:16:51 -0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
-echo "building to ${1:-dev}"
-yarn build
+git diff-index --quiet HEAD -- || echo "untracked";
 
-if [ $1 == 'prod' ]; then
-    echo "deloying to prod"
-    aws s3 sync ./build s3://www.fortniteclipz.com
 
-    IFS='. ' read -r -a array <<< $(git describe --tags $(git rev-list --tags --max-count=1))
-    array[2]="$((array[2] + 1))"
-    function join { local IFS="$1"; shift; echo "$*"; }
-    version=$(join . ${array[@]})
-    git tag -a $version -m ""
-else
-    echo "deloying to dev"
-    aws s3 sync ./build s3://dev.fortniteclipz.com
-fi
+# echo "building to ${1:-dev}"
+# yarn build
+
+# if [ $1 == 'prod' ]; then
+#     echo "deloying to prod"
+#     aws s3 sync ./build s3://www.fortniteclipz.com
+
+#     IFS='. ' read -r -a array <<< $(git describe --tags $(git rev-list --tags --max-count=1))
+#     array[2]="$((array[2] + 1))"
+#     function join { local IFS="$1"; shift; echo "$*"; }
+#     version=$(join . ${array[@]})
+#     git tag -a $version -m ""
+# else
+#     echo "deloying to dev"
+#     aws s3 sync ./build s3://dev.fortniteclipz.com
+# fi
